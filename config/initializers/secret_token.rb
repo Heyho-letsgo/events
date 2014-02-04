50f4586f8d0bdbeb8a379d37dee886b8c513271f
André Baudin-Laurencin <abaudin@gmail.com>
2014-02-04 12:31:06 +0100

    Premier commit -rails  g controller events

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Events::Application.config.secret_key_base = '6ec161afc3ba087ae9b7209d4acce24c782bbe0a5e5de6aec387cea7b25f2122882f3dbf0d0a390a5c6c06ed214b754f0e71ca0c136a1cb347d5319a5a26a155'
